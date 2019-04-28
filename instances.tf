0f05ac7e8156abae11972e48b6c8421baa5baab5
abejide001 <abejidefemi1@gmail.com>
2019-04-28 01:35:00 +0100

    modify files

--- instances.tf
+++ instances.tf
@@ -1,39 +1,3 @@
-data "aws_ami" "ubuntu3" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["database"] # Filter ami with the value "database and gets the latest one"
-  }
-
-  owners = ["622960164406"]
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["backend"] # Filter ami with the value "backend and gets the latest one"
-  }
-
-  owners = ["622960164406"]
-}
-
-data "aws_ami" "ubuntu2" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["frontend"] # Filter ami with the value "frontend and gets the latest one"
-  }
-
-  owners = ["622960164406"]
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
 
 # Create the ec2 instance for the backend
 resource "aws_instance" "backend" {
